1e8305fc9609431c75a819628a7682610e373bc6
monk-afk <tybro9@gmail.com>
2023-12-07 19:49:09 -0400

    add line output and separate positive/potential blacklist match

--- README.md
+++ README.md
@@ -1,60 +1,51 @@
 ## Chat Filter
 
+Filter blacklisted words with associative table.
+
 Created by monk <sub>(monk.moe @ [SquareOne](https://discord.gg/pE4Tu3cf23))</sub>
 
 <sup>Copyright (c) 2023, Licensed under CC0</sup>
 
-## 
+##
+This incomplete module provides a functional starting point for a chat/message filter.
 
-Compares words in a string with words from a blacklist using a trie method. Much faster than matching words from a wordlist. This module only provides enough for a functional codebase.
-
-The init.lua file will construct a trie table using the blacklist.lua. Each word is indexed by suffix -> prefix -> word.
-
-Filter method: Remove extra whitespace and padding, remove non-alphabet characters, merge gapped words, then remove duplicate letters.  The string is then split into individual words.  Single-character words are ignored.  Cross-reference word suffix, then prefix, with the similarly indexed wordlist. Example: [m]on[k] suffix and prefix matches [m]in[k].  If an index pair is found, the word is checked against the returned keys. Currently only matches words exactly.
+"***Positive Match***" uses string.match for exact words after sanity substitutions.  It removes extra whitespace and padding, non-alphabet characters, duplicate letters, and joins gapped words.  Single-character words are ignored.  Then find index pair in blacklist using suffix -> prefix -> word.
 
 Example of Suffix->Prefix table structre:
 ```lua
-k = {
-  m = {
-    "monk",
-  },
-  l = {
-    "link"
-  }
-}
-e = {
-  f = {
-    "face",
-    "frostbite",
-  }
-}
+k = { m = { "monk" },
+      l = { "link" } },
+e = { f = { "face", "frostbite" } }
 ```
-In this example, the word 'free' would return with 'face' and 'frostbite'.
 
-## Benchmark
+"***Potential Match***" traverses the entire blacklist to find words in the input string stripped of all non-alphabetic characters including spaces. This function is enabled and is only included for research or development.
+
+##
+
+**Benchmark**
 
 Intel(R) Core(TM) i3-1005G1 CPU @ 1.20GHz
 > small_log.txt (1.6M)
 ```
-lines: 100000
-words: 251864
-curse: 2
-clock: 1.030832
----------------
-lines: 100000
-words: 251864
-curse: 2
-clock: 1.030534
+Potentials enabled,		|	Potentials disabled,
+line count: 100000		|	line count: 100000
+word count: 246021		|	word count: 246021
+black list: 12			|	black list: 12
+filter hit: 16			|	filter hit: 16
+potentials: 64			|	potentials: 0
+clock time: 1.327554	|	clock time: 1.044904
 ```
-> large_logfile.txt (52M)
+> large_logfile.txt (16M)
 ```
-lines: 3000000
-words: 8096248
-curse: 87
-clock: 34.668115
-----------------
-lines: 3000000
-words: 8096248
-curse: 87
-clock: 33.885449
+Potentials enabled,		|	Potentials disabled,
+line count: 1000000		|	line count: 1000000
+word count: 2384993		|	word count: 2384993
+black list: 12			|	black list: 12
+filter hit: 63			|	filter hit: 63
+potentials: 407			|	potentials: 0
+clock time: 13.267387	|	clock time: 9.918169
 ```
+
+##
+
+**dev_0.02**
--- init.lua
+++ init.lua
@@ -1,61 +1,83 @@
-  -- Chat Filter - init.lua
-  --	0.01 - by: monk
-  --
-local osc = os.clock()
-local l, w, c = 0, 0, 0
+  -- Chat Filter - init.lua.lua
+  --	dev_0.02 - by: monk
+  -- 
+local ln, wc, ps, pn, bl, osc = 0, 0, 0, 0, 0, os.clock()  -- benchmark timer and counters
+
+local filter = {}
+local blacklist = "blacklist.lua"
+local fakechat = "./fakechat_small.txt"  -- 100 thousand lines, 1671071B
+-- local fakechat = "./fakechat_large.txt"  -- 1 million lines, 16403433B
 
 local match = string.match
 local gmatch = string.gmatch
 local lower = string.lower
 local gsub = string.gsub
 
-local curses = dofile("./blacklist.lua")
-local reference_table = {}
 
-
-local index_references = function(curses)
+local index_blacklist = function(curses)
+bl = #curses
 	for i, curse in pairs(curses) do
 		local head = match(curses[i], "^%S")
 		local tail = match(curses[i], "%S$")
 
-		if not reference_table[tail] then
-			reference_table[tail] = {}
+		if not filter[tail] then
+			filter[tail] = {}
 		end
 
-		if not reference_table[tail][head] then
-			reference_table[tail][head] = {}
+		if not filter[tail][head] then
+			filter[tail][head] = {}
 		end
 
-		table.insert(reference_table[tail][head], curse)
+		table.insert(filter[tail][head], curse)
 	end
-	return reference_table
+	return filter
 end
-index_references(curses)
+local curses = dofile(blacklist)
+index_blacklist(curses)
 
 
-local evaluate = function(head, word)
+local find_positive = function(head, word)
+	local positive = {}
+
 	for _,curse in pairs(head) do
-		if #curse == #word then
-			if curse == word then
-				c = c + 1
-			end
+
+		if curse == word then
+			table.insert(positive, word)
 		end
 	end
+	return positive
 end
 
-local reference = function(string)
+local find_potential = function(string)
+	local potential
+
+	for i = 1, #curses do
+	local curse = curses[i]
+
+		if string.find(string, curse) then
+		potential = potential or {}
+			if not potential[curse] then
+				potential[curse] = {}
+			end
+			
+			table.insert(potential[curse], line)
+		end
+	end
+	return potential
+end
+
+local find_index = function(string)
 	for word in string:gmatch("%S+") do
-	w = w + 1
-	
-		if #word > 1 then
+		wc = wc + 1
+		if #word >= 2 then
 			local tail = match(word, "%S$")
 
-			if reference_table[tail] then
+			if filter[tail] then
 				local head = match(word, "^%S")
-				head = reference_table[tail][head]
+				head = filter[tail][head]
 			
 				if head then
-					return evaluate(head, word)
+					return find_positive(head, word)
 				end
 			end
 		end
@@ -63,22 +85,30 @@ local reference = function(string)
 end
 
 
-local sweep = function(string)
-	return reference(
-		lower(string):
-			gsub("(%w+)", " %1 "): -- extra spaces
-			gsub("^%s*(.-)%s*$","%1"): -- padding
-			gsub("[^%sa-zA-Z]", ""): -- non-alpha
-			gsub("%s-(%w*)%s(%w)%s", "%1%2"): -- w o r d g a p s 
-			gsub("([%s%S])%1([%s%S]*)%2([%s%S]*)%3", "%1"): -- reppittittionns
-			gsub("([%s%S])%1", "%1") -- repititions second pass
-	)
+local string_potential = function(message)
+	string = lower(message):
+		gsub("[^a-zA-Z]", ""):  -- all non-alphabetic
+		gsub("([%s%S])%1([%s%S]*)%2([%s%S]*)%3", "%1"):  -- duplicate chars
+		gsub("([%s%S])%1", "%1")  -- duplicates 2nd pass
+
+	return find_potential(string)
 end
 
 
-local function iterate_logs()
-	local logfile = "./small_logfile.txt" -- 1.8MB, 100 thousand lines
-	-- local logfile = "./large_logfile.txt" -- 52MB, 3 million lines
+local string_positive = function(message)
+	string = lower(message):
+		gsub("(%w+)", " %1 "): -- extra spaces
+		gsub("^%s*(.-)%s*$","%1"):  -- padding
+		gsub("[^%sa-zA-Z]", ""):  -- non-alphanumeric
+		gsub("%s-(%w*)%s(%w)%s", "%1%2"):  -- 'w o r d g a p s'
+		gsub("([%s%S])%1([%s%S]*)%2([%s%S]*)%3", "%1"):  -- dduuupplicaaates
+		gsub("([%s%S])%1", "%1")  -- duplicates 2nd pass
+
+	return find_index(string)
+end
+
+
+local function simulate_chat()
 
 	local function file_exists(file)
 		local f = io.open(file,"rb")
@@ -86,20 +116,79 @@ local function iterate_logs()
 		return f~=nil
 	end
 
-	if not file_exists(logfile) then
+	if not file_exists(fakechat) then
 		return
 	end
 
-	for line in io.lines(logfile) do
-		l = l + 1
-		sweep(line)
+
+	local filtered_words = {
+		pos = {}, pot = {},
+	}
+
+	for line in io.lines(fakechat) do
+		ln = ln + 1
+
+		local positive = string_positive(line)
+
+		if positive then
+			for i = 1, #positive do
+				local curse = positive[i]
+				
+				if not filtered_words.pos[curse] then
+					filtered_words.pos[curse] = 0
+				end
+
+				filtered_words.pos[curse] = filtered_words.pos[curse] + 1
+			end
+			ps = ps + #positive
+		end
+
+
+		local potential = string_potential(line)
+
+		if potential then
+			for curse in pairs(potential) do
+
+				if not filtered_words.pot[curse] then
+					filtered_words.pot[curse] = {}
+				end
+
+				table.insert(filtered_words.pot[curse], line)
+			end
+			pn = pn + 1
+		end
 	end
+	return filtered_words
 end
 
-iterate_logs()
-	print(
-		"lines: "..l.."\n"..
-		"words: "..w.."\n"..
-		"curse: "..c.."\n"..
-		"clock: "..tonumber(string.format("%.6f", os.clock() - osc)).."\n"
-	)
+
+local filtered_words = simulate_chat()
+
+local function positive_matches()
+	print("Positive Matches:")
+	for word, count in pairs(filtered_words.pos) do
+		print("  "..word..": "..count.."\n")
+	end
+end
+positive_matches()
+
+local function potentials_found()
+	print("Potentials Found:")
+	for potential in pairs(filtered_words.pot) do
+		print(potential.." = {")
+		for count,message in pairs(filtered_words.pot[potential]) do
+			print("   "..count.." {"..message.."},")
+		end
+		print("},\n")
+	end
+end
+potentials_found()
+
+print(
+	"line count: "..ln.."\n"..
+	"word count: "..wc.."\n"..
+	"black list: "..bl.."\n"..
+	"filter hit: "..ps.."\n"..
+	"potentials: "..pn.."\n"..
+	"clock time: "..tonumber(string.format("%.6f", os.clock() - osc)).."\n"
+)
