fe6c53cfbd16f71535e3fa1e3fd98e84e0bfdb74
monk-afk <admin@monk.moe>
2023-12-18 19:35:37 -0400

    pivot update dump
    turning away from evasion patterns

--- lua_chat-filter/init.lua
+++ lua_chat-filter/init.lua
@@ -1,11 +1,15 @@
   --[[  Chat Filter (Lua Port)  ]]--
   --[[   // monk @ SquareOne    ]]--
-  --[[   init.lua - dev_0.06    ]]--
+  --[[   init.lua - dev_0.0x    ]]--
   --[[     Licensed by CC0      ]]--
+
+-- dump update because i'm pivoting away from trying to match evasion techniques
+-- it's not worth the cpu time to check for all the evasion techniques.
 local match = string.match
 local gmatch = string.gmatch
 local lower = string.lower
 local gsub = string.gsub
+local rep = string.rep
 
 
 local filter = {
@@ -44,25 +48,6 @@ end
 index_filter()
 
 
-local function remove_links(string)
-	return gsub(string, "[http]*[s]*[:/]*(%w[-.%w]*%.).+([.%S%S]*%S*)", "")
-end
-
-local function remove_symbols(string)
-	return gsub(string, "[^%sa-zA-Z]", "")
-end
-
-local function remove_space(string, trim)
-	trim = trim and " " or ""
-	return gsub(string, "%s+", trim)
-end
-
-local function remove_repeating(string)
-	return gsub(string,"([%s%S])%1([%s%S]*)%2([%s%S]*)%3", "%1"):
-			gsub("([%s%S])%1", "%1")
-end
-
-
 local function try_blacklist(word)
 	local tail = match(word, "%S$")
 	local head = match(word, "^%S")
@@ -70,8 +55,8 @@ local function try_blacklist(word)
 	local blacklist_keys = blacklist[tail] and blacklist[tail][head]
 
 	if blacklist_keys then
-		for _,listed_item in pairs(blacklist_keys) do
-			if word == listed_item then
+		for n = 1, #blacklist_keys do
+			if word == blacklist_keys[n] then
 				word = string.rep("*", word:len())
 
 				return word
@@ -82,9 +67,89 @@ local function try_blacklist(word)
 end
 
 
-local fakechat = "fakechat.txt"  -- Replace with input stream from here
+local function remove_links(string)
+	return gsub(string, "(h*t*t*p*s*:*/*/*%S+%./*%S*)", "(%1)")
+end
 
-local function chat_stream()
+
+-- local function remove_nonalpha(split_string)
+-- 		-- return gsub(string, "[^%sa-zA-Z]", "")
+
+-- 	for n = 1, #split_string do
+
+-- 	end
+
+-- 	return
+-- end
+
+
+local function remove_repeats(word_table)
+	for n = 1, #word_table do
+		local no_repeats = {}
+		for c = 1, #word_table[n] do
+		if not no_repeats[n] then
+			no_repeats[n] = ""
+		end
+			local alpha = string.sub(word_table[n], c, c)
+			local beta = string.sub(word_table[n], c+1, c+1)
+			if alpha ~= beta then
+				no_repeats[n] = no_repeats[n] .. alpha
+			end
+		end
+		word_table[n] = no_repeats[n]
+	end
+
+	return word_table
+end
+
+
+local merge_gaps = function(word_table)
+	local words, i = {}, 1
+	for n = 1, #word_table do
+		if not words[n] then
+			words[n] = ""
+		end
+
+		if not word_table[i] then
+			break
+		end
+
+		local c = 0
+		if #word_table[i] == 1 then
+			for b = i,#word_table do
+				if #word_table[b] ~= 1 then
+					break
+				end
+				words[n] = words[n] .. word_table[b]
+				c = c + 1
+			end
+		else
+			words[n] = word_table[i]
+			c = c + 1
+		end
+		i = i + c
+	end
+	return words
+end
+
+
+local function make_word_table(string)
+	local word_table, n = {}, 1
+
+	gsub(string, "%S+", function(word)
+		if not word_table[n] then
+			word_table[n] = ""
+		end
+		
+		word_table[n] = word
+		n = n + 1
+	end)
+	return word_table
+end
+
+
+local fakechat = "fakechat.txt"  -- Replace with input stream from here
+local function simulate_chat()
 
 	local function file_exists(file)
 		local f = io.open(file,"rb")
@@ -97,40 +162,28 @@ local function chat_stream()
 	end
 
 	for message in io.lines(fakechat) do  -- replace fakechat with real chat
-		local string = message:lower()
+		print(os.date("%b-%d %H:%M:%S"))
+		local string = message --:lower()
+
+		if string:len() > 28 then
+			string:lower()
+		end
 
 		string = remove_links(string)
-		-- string = remove_gaps(string)  -- needs fix
-		string = remove_repeating(string)
-		
 
-		local n, s = 1, {}
-		local word, num = gsub(string:lower(), "%S+", 
-		function(word)
-			if #word > 1 then
-				
-				local no_symbol = gsub(word, "%A", "")	
-				if no_symbol then
-					word = try_blacklist(no_symbol)
-				end
+		local word_table = {}
+		word_table = make_word_table(string)
+		-- 
+		-- merge_gaps and remove_repeats don't work the best together
+		word_table = merge_gaps(word_table)
+		word_table = remove_repeats(word_table)
 
-				if gsub(word, "(%S%S+)%1", "%1") then
-					word = try_blacklist(word)
-				end
-				
-				word = try_blacklist(word)
-			end
-			
-			table.insert(s, n, word)
-			n=n+1
-		end)
 
-		message = table.concat(s, " ")
-	return message
+		return table.concat(word_table, " ")
 	end
 end
 
 
-local send_message = chat_stream()
+local send_message = simulate_chat()
 
-print(send_message)
+print("X: "..send_message)
