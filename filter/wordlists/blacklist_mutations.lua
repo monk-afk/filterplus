c1cea6d3c085a6f4025bc94ed5104e94d6164dff
monk <admin@monk.moe>
2026-06-19 18:11:19 -0300

    add fat finger mapping to use with the Norvig methods

--- filter/wordlists/blacklist_mutations.lua
+++ filter/wordlists/blacklist_mutations.lua
@@ -1,43 +1,67 @@
--- The following code is used under license:
-  -- https://github.com/pakozm/lua-spell-correct
-  -- Copyright 2014 Francisco Zamora-Martinez
-    -- Adaptation of Peter Norvig python Spelling Corrector:
+  -- Adaptation of Peter Norvig's python Spelling Corrector:
     -- http://norvig.com/spell-correct.html
-    -- Open source code under MIT license: http://www.opensource.org/licenses/mit-license.php
+  -- Original code by:
+    -- https://github.com/pakozm/lua-spell-correct
+    -- Copyright 2014 Francisco Zamora-Martinez
 
-
--- Some blacklisted words are especially prone to obfuscation by players.
--- Instead of hand-curating hundreds of variants this file automatically generates them
--- Use sparringly, each word added to this list will generate
-  --  25 + (54 * (word:len() - 1)) additional blacklist words
+-- Instead of hand-curating hundreds of curse variants
+-- this file automatically generates them based on key placements
 
 local function mutate()
-  local alphabet = {}
-  for a in ("abcdefghijklmnopqrstuvwxyz"):gmatch(".") do
-    alphabet[#alphabet+1] = a
-  end
+  local fat_finger_map = {
+    a = {"q","w","s","z","x"},
+    b = {"v","g","h","n"},
+    c = {"x","d","f","v"},
+    d = {"s","e","r","f","c","x"},
+    e = {"w","s","d","r"},
+    f = {"d","r","t","g","v","c"},
+    g = {"f","t","y","h","b","v"},
+    h = {"g","y","u","j","n","b"},
+    i = {"u","j","k","o"},
+    j = {"h","u","i","k","n","m"},
+    k = {"j","i","o","l","m"},
+    l = {"k","o","p"},
+    m = {"n","j","k"},
+    n = {"b","h","j","m"},
+    o = {"i","k","l","p"},
+    p = {"o","l"},
+    q = {"w","a"},
+    r = {"e","d","f","t"},
+    s = {"a","w","e","d","x","z"},
+    t = {"r","f","g","y"},
+    u = {"y","h","j","i"},
+    v = {"c","f","g","b"},
+    w = {"q","a","s","e"},
+    x = {"z","s","d","c","a"},
+    y = {"t","g","h","u"},
+    z = {"a","s","x"},
+  }
 
-  local mutant_list = {
+  -- SEE FILE: mutation_exceptions.lua
+  local blacklist = {
     "bitch",
     "dick",
     "fuck",
+    "fucker",
     "fucking",
     "nigger",
     "shit",
     "puta",
     "sexy",
-    "slut",
     "faggot",
     "asshole"
   }
+
   local mutations = {}
 
-  for _,word in ipairs(mutant_list) do
+  for _,word in ipairs(blacklist) do
     local partials = {}
     local letters = {}
+    local position_map = {}
 
     for i = 1, #word do
       letters[i] = word:sub(i,i)
+      position_map[i] = fat_finger_map[letters[i]] or {}
       -- paires parts of the word: {"f","uck"}, {"fu","ck"}, {"fuc","k"}
       partials[i] = {word:sub(1,i), word:sub(i)}
     end
@@ -58,15 +82,17 @@ local function mutate()
 
     -- replacements (fack, fbck)
     for i = 2, #word do
-      for j = 1, #alphabet do
-        table.insert(mutations, partials[i-1][1] .. alphabet[j] .. partials[i+1][2])
+      local mapped = position_map[i]
+      for j = 1, #mapped do
+        table.insert(mutations, partials[i-1][1] .. mapped[j] .. partials[i+1][2])
       end
     end
 
     -- insertions (fauck, fbuck)
     for i = 1, #word do
-      for j = 1, #alphabet do
-        table.insert(mutations, partials[i][1] .. alphabet[j] .. partials[i+1][2])
+      local mapped = position_map[i]
+      for j = 1, #mapped do
+        table.insert(mutations, partials[i][1] .. mapped[j] .. partials[i+1][2])
       end
     end
   end
@@ -75,3 +101,26 @@ local function mutate()
 end
 
 return mutate()
+------------------------------------------------------------------------------------
+-- MIT License                                                                    --
+--                                                                                --
+-- Copyright © 2026 monk (https://github.com/monk-afk)                            --
+--                                                                                --
+-- Permission is hereby granted, free of charge, to any person obtaining a copy   --
+-- of this software and associated documentation files (the "Software"), to deal  --
+-- in the Software without restriction, including without limitation the rights   --
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      --
+-- copies of the Software, and to permit persons to whom the Software is          --
+-- furnished to do so, subject to the following conditions:                       --
+--                                                                                --
+-- The above copyright notice and this permission notice shall be included in all --
+-- copies or substantial portions of the Software.                                --
+--                                                                                --
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     --
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       --
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    --
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         --
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  --
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  --
+-- SOFTWARE.                                                                      --
+------------------------------------------------------------------------------------
